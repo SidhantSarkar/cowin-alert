16a3fdac66333d0a40ec17f558afc9416ccf76bf
SidhantSarkar <sidhantssarkar25@gmail.com>
2021-06-06 01:06:06 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,114 @@
+const container = document.querySelector(".container");
+
+const showCoffees = () => {
+
+  let output = "";
+  this.localforage.getItem('pincode_data').then((value) => {
+    if (!value) return;
+    value['centers'].forEach(center => {
+      output += `
+              <div class="card">
+                <h1 class="card--title">${center.name}</h1>
+                <h4>Address: ${center.address}</h4>
+                <a class="card--link" href="#">Book Now</a>
+              </div>
+              `
+    });
+    container.innerHTML = output;
+
+  }).catch(function (err) {
+    console.log(err);
+  });
+
+};
+
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+
+    const params = new URLSearchParams(self.window.location.search);
+    const myParam = params.get('userid') || 'sidhant';
+
+    fetch(`/data/${myParam}.json`)
+      .then(response => response.json())
+      .then(res => {
+        localforage.setItem('user_config', res).then(() => {
+
+          navigator.serviceWorker
+            .register('/myWorker.js')
+            .then(() => {
+              console.log("service worker registered");
+            })
+            .catch(err => console.log("service worker not registered", err));
+
+
+        }).catch(function (err) {
+          // This code runs if there were any errors
+          console.log(err);
+        });
+      })
+      .catch(err => console.log(err));
+
+  });
+}
+
+// const askPerm = async () => {
+//   console.log('Here Before');
+//   const status = await navigator.permissions.query({
+//     name: 'periodic-background-sync',
+//   });
+//   if (status.state === 'granted') {
+//     // Periodic background sync can be used.
+//     console.log('Here');
+//   } else {
+//     // Periodic background sync cannot be used.
+//   }
+// }
+
+// async function registerPeriodicNewsCheck() {
+//   if ('serviceWorker' in navigator) {
+//     const registration = await navigator.serviceWorker.ready;
+//     // Check if periodicSync is supported
+//     if ('periodicSync' in registration) {
+//       // Request permission
+//       const status = await navigator.permissions.query({
+//         name: 'periodic-background-sync',
+//       });
+//       if (status.state === 'granted') {
+//         try {
+//           // Register new sync every 24 hours
+//           await registration.periodicSync.register('get-updates', {
+//             minInterval: 60, // 1 day
+//           });
+//           console.log('Periodic background sync registered!');
+//         } catch (e) {
+//           console.error(`Periodic background sync failed:\n${e}`);
+//         }
+//       }
+//     }
+//   }
+
+//   navigator.serviceWorker.ready.then(registration => {
+//     registration.periodicSync.getTags().then(tags => {
+//       if (tags.includes('get-updates')) console.log("Chall Gaya");
+//       // skipDownloadingLatestNewsOnPageLoad();
+//     });
+//   });
+// }
+
+
+
+let requestPermission = () => {
+  if ('Notification' in window && navigator.serviceWorker) {
+    // Display the UI to let the user toggle notifications
+    Notification.requestPermission(function (status) {
+      console.log('Notification permission status:', status);
+    });
+  }
+
+}
+
+
+
+// askPerm();
---
+++ myWorker.js
@@ -0,0 +1,91 @@
+importScripts('js/localforage.js');
+importScripts('js/loadash.js');
+
+self.addEventListener("install", () => {
+    this.localforage.getItem('user_config').then((value) => {
+        self.userConfig = value;
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+
+self.skipWaiting();
+
+self.addEventListener("activate", () => {
+    setInterval(() => {
+        self.sentAlert = false;
+        const currDate = new Date().toLocaleDateString('en-GB').replaceAll("/", "-");
+        fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${self.userConfig.district}&date=${currDate}`)
+            .then(response => response.json())
+            .then(res => {
+                this.localforage.getItem('district_data').then((value) => {
+                    if (value && _.isEqual(value, res)) return;
+                    this.localforage.setItem('district_data', { ...res, 'last_update': new Date().toISOString() });
+                    if (self.sentAlert) return;
+                    var options = {
+                        body: 'New slots available in your selected District.',
+                        icon: 'images/icons/icon-72x72.png',
+                        vibrate: [100, 50, 100],
+                    };
+                    self.registration.showNotification('Vaccination Slots Found!', options);
+                    self.sentAlert = true;
+                }).catch(function (err) {
+                    console.log(err);
+                });
+            })
+            .catch(err => console.error(err));
+        fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${self.userConfig.pincode}&date=${currDate}`)
+            .then(response => response.json())
+            .then(res => {
+                this.localforage.getItem('pincode_data').then((value) => {
+                    if (value && _.isEqual(value, res)) return;
+                    this.localforage.setItem('pincode_data', { ...res, 'last_update': new Date().toISOString() });
+                    if (self.sentAlert) return;
+                    var options = {
+                        body: 'New slots available in your selected Pincode.',
+                        icon: 'images/icons/icon-72x72.png',
+                        vibrate: [100, 50, 100],
+                    };
+                    self.registration.showNotification('Vaccination Slots Found!', options);
+                    self.sentAlert = true;
+                }).catch(function (err) {
+                    console.log(err);
+                });
+            })
+            .catch(err => console.error(err));
+    }, 30 * 1000);
+})
+// self.addEventListener('periodicsync', event => {
+//     event.waitUntil(() => {
+//         console.log("Test");
+//         return;
+//     });
+//     // console.log("Here before");
+//     // if (event.tag !== 'get-updates') return;
+//     // else {
+//     //     console.log("Here");
+//     //     var options = {
+//     //         body: 'This notification was generated from a push!',
+//     //         icon: 'images/icons/icon-72x72.png',
+//     //         vibrate: [100, 50, 100],
+//     //         data: {
+//     //             dateOfArrival: Date.now(),
+//     //             primaryKey: '1'
+//     //         },
+//     //         actions: [
+//     //             {
+//     //                 action: 'explore', title: 'Explore this new world',
+//     //                 icon: 'images/coffee1.jpg'
+//     //             },
+//     //             {
+//     //                 action: 'close', title: 'Close',
+//     //                 icon: 'images/coffee2.jpg'
+//     //             },
+//     //         ]
+//     //     };
+//     //     self.registration.showNotification('Hello world!', options);
+// });
+
+self.addEventListener('fetch', function (event) {
+    // it can be empty if you just want to get rid of that error
+});
